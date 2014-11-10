1f196b8be598852b48194ba9f481fcf9119fef84
Lukas Abegg <lukas@Lukass-MBP.fritz.box>
2014-11-10 19:03:17 +0100

    javadoc changes for final version

--- src/de/bht/fpa/mail/s798972/controller/MainViewController.java
+++ src/de/bht/fpa/mail/s798972/controller/MainViewController.java
@@ -1,25 +1,22 @@
 package de.bht.fpa.mail.s798972.controller;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
 /**
- * Controller for main application builded 
- * with FXML document FXMLMainView
- * 
+ * Controller for main application builded with FXML document FXMLMainView
+ *
  * @author Lukas Abegg, S53647, FPA - Beuth Hochschule
  * @version 1.0
  */
 public class MainViewController implements Initializable {
 
     /**
-     * Call initiation methods and set default parameters
-     * for main view
-     * 
+     * Call initiation methods and set default parameters for main view
+     *
      * @param url application url
-     * @param rb bundle for handling language translations  
+     * @param rb bundle for handling language translations
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
